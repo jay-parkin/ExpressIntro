fae235c93d68a3f044b26cd8cc894ab04f49006d
Jason Parkin <jay-parkin91@hotmail.com>
2024-10-15 20:01:44 +1100

    feat: server hello world implemented

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+// make an instance of the express system
+// So we can configure it
+// eg, routes, settings
+const app = express();
+
+// GET localhost:3000/
+app.get("/", (request, response) => {
+  //sends html
+  //   response.send("<h1>Hello world!<h1>");
+
+  // sends json
+  response.json({
+    message: "Hello World!",
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on localhost:${PORT}`);
+});
